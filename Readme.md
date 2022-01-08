bdb5a184fdceddbf479b65e0d4bc47ae104362ca
Dylan Steele <dylansteele57@gmail.com>
2022-01-07 20:36:48 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Angular Nebular / Nestjs Template
+
+This template is a mono-repo setup with rushjs, checkout that out [here](https://rushjs.io/), with an Angular with Nebular-UI frontend and a Nestjs api backend.
+
+## Getting Started
+
+update and install dependencies
+
+```bash
+rush update
+```
---
+++ apps/frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbButtonModule, NbIconModule, NbActionsModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([], { relativeLinkResolution: 'legacy' }),
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbButtonModule,
+    NbIconModule,
+    FormsModule,
+    NbActionsModule,
+
+    // app
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
