fd79b150dcf0c8512445c639d427cfa947b05609
Dylan Steele <dylansteele57@gmail.com>
2022-01-10 18:38:45 -0500

    frontend: added custom font, moving off Nebular

--- apps/frontend/src/app/features/home/home.module.ts
+++ apps/frontend/src/app/features/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { NbCardModule, NbIconModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule]
+  imports: [CommonModule, HomeRoutingModule, NbIconModule, NbCardModule]
 })
 export class HomeModule {}
