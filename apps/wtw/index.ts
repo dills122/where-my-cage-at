e003124bae15a17baabd10276bbe9f0fa6c5f90d
Dylan Steele <dylansteele57@gmail.com>
2022-01-14 22:52:16 -0500

    wtw: started search endpoint

--- apps/wtw/index.ts
+++ apps/wtw/index.ts
@@ -1,12 +1,14 @@
 import got, { OptionsOfJSONResponseBody } from 'got';
 import camelCaseObject from 'camelcase-keys';
 import { catchError, from, map, Observable, throwError } from 'rxjs';
+import _ from 'lodash';
 
 const API_DOMAIN = 'https://apis.justwatch.com/content';
 
 export interface RequestArgs {
   url: string;
   method: 'GET' | 'POST';
+  querySearchTerms?: QuerySearchTerms;
 }
 
 export default class WTW {
@@ -15,14 +17,52 @@ export default class WTW {
     this._locale = locale;
   }
 
-  private request<T>({ url, method }: RequestArgs): Observable<T> {
+  private setupDefaultQuerySearchTerms() {
+    return {
+      fields: [
+        'full_path',
+        'full_paths',
+        'id',
+        'localized_release_date',
+        'object_type',
+        'poster',
+        'scoring',
+        'title',
+        'tmdb_popularity',
+        'backdrops',
+        'production_countries',
+        'offers',
+        'original_release_year',
+        'backdrops'
+      ]
+    };
+  }
+
+  private setupSearchParams(querySearchTerms: QuerySearchTerms) {
+    querySearchTerms = {
+      ...this.setupDefaultQuerySearchTerms(),
+      ...querySearchTerms
+    };
+    for (const key in querySearchTerms) {
+      if (Object.prototype.hasOwnProperty.call(querySearchTerms, key) && _.isArray(querySearchTerms[key])) {
+        querySearchTerms[key] = JSON.stringify(querySearchTerms[key]);
+      }
+    }
+    return querySearchTerms as Record<string, string>;
+  }
+
+  private request<T>({ url, method, querySearchTerms }: RequestArgs): Observable<T> {
+    if (querySearchTerms) {
+      console.log(this.setupSearchParams(querySearchTerms));
+    }
     return from(
       got<object>(url, {
         prefixUrl: API_DOMAIN,
         headers: {
           ['User-Agent']: 'JustWatch client (https://github.com/dills122/where-my-cage-at/apps/wtw)'
         },
-        method
+        method,
+        searchParams: !querySearchTerms ? undefined : this.setupSearchParams(querySearchTerms)
       } as OptionsOfJSONResponseBody)
     ).pipe(
       map(({ body }) => {
@@ -30,6 +70,14 @@ export default class WTW {
       }),
       catchError((err) => throwError(() => new Error(err)))
     );
+  }
+
+  search(querySearchTerms: QuerySearchTerms) {
+    return this.request<ServiceProvider[]>({
+      url: `titles/${this._locale}/popular`,
+      method: 'GET',
+      querySearchTerms
+    });
   }
 
   getProviders() {
@@ -40,6 +88,25 @@ export default class WTW {
   }
 }
 
+//TODO probably needs updated to fix with better more accurate types
+export interface QuerySearchTerms {
+  content_types?: string;
+  presentation_types?: string;
+  providers?: string;
+  genres?: string[];
+  languages?: string[];
+  release_year_from?: string;
+  release_year_until?: string;
+  monetization_types?: string;
+  min_price?: string;
+  max_price?: string;
+  scoring_filter_types?: string;
+  cinema_release?: string;
+  query?: string;
+  page?: number;
+  page_size?: number;
+  fields?: string[];
+}
 export interface ServiceProvider {
   id: number;
   technicalName: string;
