db7d89d6e75bac385d526e632a326a238a88b17d
Dylan Steele <dylansteele57@gmail.com>
2022-01-18 22:29:56 -0500

    infa: updated docker configs for ports (#6)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../.env' });
+
+const PORT = process.env.API_PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(Number(PORT));
 }
 bootstrap();
