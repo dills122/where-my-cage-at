57429985f35fccc20be22bb2930e748a1c3694db
Dylan Steele <dylansteele57@gmail.com>
2022-01-25 15:34:25 -0500

    api: added throttling, helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { fastifyHelmet } from 'fastify-helmet';
 import {
-  FastifyAdapter,
-  NestFastifyApplication,
+	FastifyAdapter,
+	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
@@ -12,15 +13,16 @@ dotenv.config({ path: __dirname + '/../.env' });
 const PORT = process.env.API_PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({
-      logger: true,
-    }),
-  );
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(Number(PORT), '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}`);
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter({
+			logger: true,
+		}),
+	);
+	await app.register(fastifyHelmet);
+	app.enableCors();
+	app.useGlobalPipes(new ValidationPipe());
+	await app.listen(Number(PORT), '0.0.0.0');
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
