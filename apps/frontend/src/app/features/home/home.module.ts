a32f44b4150a2873d6227a515ab933ad3e9a30cc
Dylan Steele <dylansteele57@gmail.com>
2022-01-26 18:26:11 -0500

    frontend: image lazy loading, icons

--- apps/frontend/src/app/features/home/home.module.ts
+++ apps/frontend/src/app/features/home/home.module.ts
@@ -8,9 +8,10 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DividerModule } from 'primeng/divider';
 import { ServiceButtonNodeComponent } from './components/service-button-node/service-button-node.component';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @NgModule({
   declarations: [HomeComponent, ServiceButtonNodeComponent],
-  imports: [CommonModule, HomeRoutingModule, ButtonModule, CardModule, DividerModule]
+  imports: [CommonModule, HomeRoutingModule, ButtonModule, CardModule, DividerModule, LazyLoadImageModule]
 })
 export class HomeModule {}
