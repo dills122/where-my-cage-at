6798b8908726896aad6a1480d866bf5e057e21dc
Dylan Steele <dylansteele57@gmail.com>
2022-01-26 16:09:56 -0500

    api: added endpoint for getting icons

--- apps/api/src/service-providers/controller/service-providers.controller.ts
+++ apps/api/src/service-providers/controller/service-providers.controller.ts
@@ -1,15 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	HttpException,
+	HttpStatus,
+	Param,
+	StreamableFile,
+	Response,
+} from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 import { ServiceProvider } from 'redis-sdk';
 import { ServiceProvidersService } from '../service/service-providers.service';
 
 @Controller('service-providers')
 export class ServiceProvidersController {
-  constructor(
-    private readonly serviceProviderService: ServiceProvidersService,
-  ) {}
+	constructor(
+		private readonly serviceProviderService: ServiceProvidersService,
+	) {}
 
-  @Get()
-  async findAll(): Promise<ServiceProvider[]> {
-    return this.serviceProviderService.getAll();
-  }
+	@Get()
+	async findAll(): Promise<ServiceProvider[]> {
+		return this.serviceProviderService.getAll();
+	}
+
+	@Get('icon/:id')
+	getServiceIcon(
+		@Param() params,
+		@Response({ passthrough: true }) res,
+	): StreamableFile {
+		const { id } = params;
+		if (!id || Number(id) <= 0) {
+			throw new HttpException('No recordId provided', HttpStatus.BAD_REQUEST);
+		}
+		//TODO need to update this with a prod v. dev file path
+		const file = createReadStream(
+			join(process.cwd(), '..', '..', '..', 'data', 'icons', `${id}.webp`),
+		);
+		res.set({
+			'Content-Type': 'image/webp',
+			'Content-Disposition': `attachment; filename="${id}.webp"`,
+		});
+		return new StreamableFile(file);
+	}
+	//TODO create an endpoint that zips all of the images and sends as one package
 }
