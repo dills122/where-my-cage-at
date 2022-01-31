bdad4a146584f99c2ecdf211713d7ff164e1e1ad
Dylan Steele <dylansteele57@gmail.com>
2022-01-31 15:27:25 -0500

    infa: finshed cert script, fixed cloud config

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo certbot --nginx -d wheremycageat.com -d www.wheremycageat.com -d api.wheremycageat.com -d www.api.wheremycageat.com
+local domain = "wheremycageat.com"
+local api_sub_domain = "api"
+local ssl_email = "dylansteele57@gmail.com"
+
+mkdir -p /var/www/prod
+
+sudo certbot --nginx --non-interactive --webroot -w /var/www/prod/ --agree-tos -d ${domain} -d www.${domain} -d ${api_sub_domain}.${domain} -d www.${api_sub_domain}.${domain} -m ${ssl_email}
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 # Should only be run in a droplet
 
+mkdir -p /var/www/prod
+
 # Copy Nginx config files
 cp ~/app-src/.docker/nginx/conf.d/* /etc/nginx/conf.d/
 
@@ -10,7 +12,6 @@ pushd ~/app-src/
 # Setup & Deploy Frontend
 rush update
 rush build --to frontend
-mkdir /var/www/prod
 
 rush deploy --project frontend
 cp ./common/deploy/apps/frontend/dist/frontend/* /var/www/prod
@@ -18,6 +19,9 @@ cp ./common/deploy/apps/frontend/dist/frontend/* /var/www/prod
 # Get all of the docker services running
 docker compose up -d
 
+# Might be dup commands
 sudo systemctl restart nginx
+
+nginx -t && nginx -s reload
 
 popd
