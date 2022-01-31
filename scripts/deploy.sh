235fe12daba8a14e5c9864cb9dd09dbb627fdc57
Dylan Steele <dylansteele57@gmail.com>
2022-01-31 10:56:50 -0500

    infa: updated deploy script, added env checks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# Should only be run in a droplet
+
 # Copy Nginx config files
 cp ~/app-src/.docker/nginx/conf.d/* /etc/nginx/conf.d/
+
+pushd ~/app-src/
 
 # Setup & Deploy Frontend
 rush update
 rush build --to frontend
 mkdir /var/www/prod
-rush deploy --project frontend --target-folder /var/www/prod
+
+rush deploy --project frontend
+cp ./common/deploy/apps/frontend/dist/frontend/* /var/www/prod
 
 # Get all of the docker services running
 docker compose up -d
 
 sudo systemctl restart nginx
+
+popd
