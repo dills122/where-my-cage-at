27ced7d2e206e707bcfabe259c03e9442db5aff2
Dylan Steele <dylansteele57@gmail.com>
2022-01-31 21:55:04 -0500

    infa: fixes

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-local domain = "wheremycageat.com"
-local api_sub_domain = "api"
-local ssl_email = "dylansteele57@gmail.com"
+func() {
+    local domain = "wheremycageat.com"
+    local api_sub_domain = "api"
+    local ssl_email = "dylansteele57@gmail.com"
 
-mkdir -p /var/www/prod
+    mkdir -p /var/www/prod
 
-sudo certbot --nginx --non-interactive --webroot -w /var/www/prod/ --agree-tos -d ${domain} -d www.${domain} -d ${api_sub_domain}.${domain} -d www.${api_sub_domain}.${domain} -m ${ssl_email}
+    sudo certbot --nginx --non-interactive --webroot -w /var/www/prod/ --agree-tos -d ${domain} -d www.${domain} -d ${api_sub_domain}.${domain} -d www.${api_sub_domain}.${domain} -m ${ssl_email}
+}
+
+func
--- scripts/temp-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/dills122/where-my-cage-at.git app-src
-pushd app-src
-git pull
-git checkout deployment-setup
-popd
