baf1dcd59b9cc9371548b7b142f2eb0efb975aa9
Dylan Steele <dylansteele57@gmail.com>
2022-02-01 22:04:50 -0500

    api: fixed redis host name by env

--- apps/api/src/shared/base-redis.service.ts
+++ apps/api/src/shared/base-redis.service.ts
@@ -1,21 +1,23 @@
 import { ReadOnlyClient } from 'redis-sdk';
 
 export default class RedisServiceBase {
-  protected client: ReadOnlyClient;
-  protected isConnected = false;
-  constructor() {
-    this.client = new ReadOnlyClient({
-      host: 'localhost',
-      port: '6379',
-    });
-  }
-  protected async connect() {
-    await this.client.connect();
-    this.isConnected = true;
-  }
+	protected client: ReadOnlyClient;
+	protected isConnected = false;
+	constructor() {
+		const ENV = process.env.NODE_ENV;
+		const isProd = ENV === 'prod';
+		this.client = new ReadOnlyClient({
+			host: isProd ? 'redis' : 'localhost',
+			port: '6379',
+		});
+	}
+	protected async connect() {
+		await this.client.connect();
+		this.isConnected = true;
+	}
 
-  protected async disconnect() {
-    await this.client.disconnect();
-    this.isConnected = false;
-  }
+	protected async disconnect() {
+		await this.client.disconnect();
+		this.isConnected = false;
+	}
 }
