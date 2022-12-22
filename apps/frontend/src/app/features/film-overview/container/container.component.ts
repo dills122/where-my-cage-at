4d25a15260dbb0596a293fede6e961ffef9fb673
Dylan Steele <dylansteele57@gmail.com>
2022-12-21 22:55:22 -0500

    frontend: got initial setup of film overview page

--- apps/frontend/src/app/features/film-overview/container/container.component.ts
+++ apps/frontend/src/app/features/film-overview/container/container.component.ts
@@ -2,7 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil, tap } from 'rxjs';
 import { MovieRecord } from 'src/app/models';
-import { FilmographyRepository } from 'src/app/repositories';
+import { FilmographyRepository, ServiceProviderRepository } from 'src/app/repositories';
+import { MonetizationTypes } from '../../service-provider-overview/service-providers-monetization-types-mapping';
 
 @Component({
 	selector: 'app-film-overview-container',
@@ -13,8 +14,13 @@ export class ContainerComponent implements OnInit, OnDestroy {
 	filmId: number;
 	filmRecord: MovieRecord | undefined;
 	private notifier = new Subject();
+	MonetizationTypes = MonetizationTypes;
 
-	constructor(private filmographyRepository: FilmographyRepository, private route: ActivatedRoute) {
+	constructor(
+		private filmographyRepository: FilmographyRepository,
+		private serviceProviderRepository: ServiceProviderRepository,
+		private route: ActivatedRoute
+	) {
 		this.filmId = Number(this.route.snapshot.paramMap.get('filmId') || '');
 	}
 	ngOnDestroy(): void {
@@ -28,8 +34,39 @@ export class ContainerComponent implements OnInit, OnDestroy {
 				takeUntil(this.notifier),
 				tap(record => {
 					this.filmRecord = record;
+					console.log(this.filmRecord);
 				})
 			)
 			.subscribe();
+	}
+
+	getGenres() {
+		return this.filmRecord?.genres;
+	}
+
+	getViewingOptionsBasedOnViewingPreference(viewingPreference: MonetizationTypes) {
+		const providerIds = this.filmRecord?.offers
+			.filter(offer => offer.monetizationType === viewingPreference)
+			.map(offer => offer.providerId);
+		const providerIdsSet = new Set(providerIds || []);
+		return this.serviceProviderRepository.getSubsetOfProviders([...providerIdsSet]);
+	}
+
+	//TODO need to make this so each type/resolution link is available somehow
+	getFirstUrlForServiceProvider(serviceProviderId: number, monetizationType: MonetizationTypes) {
+		const [first] =
+			this.filmRecord?.offers
+				.filter(
+					offer => offer.providerId === serviceProviderId && offer.monetizationType === monetizationType
+				)
+				.map(offer => offer.urls?.standardWeb) || [];
+		return first;
+	}
+
+	checkForProvidersWithDesiredMonitizationModel(monetizationType: MonetizationTypes) {
+		if (!this.filmRecord?.offers) {
+			return false;
+		}
+		return this.filmRecord?.offers.filter(offer => offer.monetizationType === monetizationType).length > 0;
 	}
 }
--- apps/frontend/src/app/features/film-overview/film-overview.module.ts
+++ apps/frontend/src/app/features/film-overview/film-overview.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './container/container.component';
 import { FilmOverviewRoutingModule } from './film-overview-routing.module';
+import { CardModule } from 'primeng/card';
+import { SharedModule } from '../shared/shared.module';
+import { DividerModule } from 'primeng/divider';
+import { ChipModule } from 'primeng/chip';
 
 @NgModule({
 	declarations: [ContainerComponent],
-	imports: [CommonModule, FilmOverviewRoutingModule]
+	imports: [CommonModule, CardModule, ChipModule, DividerModule, SharedModule, FilmOverviewRoutingModule]
 })
 export class FilmOverviewModule {}
